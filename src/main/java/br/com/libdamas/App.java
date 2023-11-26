49adfeff6e1e7a23654f5a1c37b0b334650192de
joaofdurao <devjoaofelipe.dlc@gmail.com>
2023-11-26 11:24:55 -0300

    first commit

---
+++ src/main/java/br/com/libdamas/App.java
@@ -0,0 +1,13 @@
+package br.com.libdamas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
