8e96bdd49895437ff686498bb81397bd25abbba4
Ivson Gabriel Feitosa da Silva <133917722+Ivson19@users.noreply.github.com>
2023-11-28 16:14:49 -0300

    Criando Models

--- src/main/java/br/com/libdamas/models/Book.java
+++ src/main/java/br/com/libdamas/models/Book.java
@@ -1,5 +1,78 @@
 package br.com.libdamas.models;
 
 public class Book {
-    
+    private long id;
+    private char title;
+    private char author;
+    private char year;
+    private char genre;
+    private boolean avaible;
+    private User currentOwner;
+
+    public Book(long id, char title, char author, char year, char genre, boolean avaible, User currentOwner) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.avaible = avaible;
+        this.currentOwner = currentOwner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public char getTitle() {
+        return title;
+    }
+
+    public void setTitle(char title) {
+        this.title = title;
+    }
+
+    public char getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(char author) {
+        this.author = author;
+    }
+
+    public char getYear() {
+        return year;
+    }
+
+    public void setYear(char year) {
+        this.year = year;
+    }
+
+    public char getGenre() {
+        return genre;
+    }
+
+    public void setGenre(char genre) {
+        this.genre = genre;
+    }
+
+    public boolean isAvaible() {
+        return avaible;
+    }
+
+    public void setAvaible(boolean avaible) {
+        this.avaible = avaible;
+    }
+
+    public User getCurrentOwner() {
+        return currentOwner;
+    }
+
+    public void setCurrentOwner(User currentOwner) {
+        this.currentOwner = currentOwner;
+    }
+
 }
--- src/main/java/br/com/libdamas/models/Loan.java
+++ src/main/java/br/com/libdamas/models/Loan.java
@@ -1,5 +1,92 @@
 package br.com.libdamas.models;
 
+import java.util.List;
+import java.util.Date;
+
 public class Loan {
-    
+    private long id;
+    private User ownerUser;
+    private Book borrowedBook;
+    private List<Book> userBooks;
+    private Date loanDate;
+    private Date returnDate;
+    private boolean closed;
+    private boolean overdue;
+
+    public Loan(long id, User ownerUser, Book borrowedBook, List<Book> userBooks, Date loanDate, Date returnDate,
+            boolean closed, boolean overdue) {
+        this.id = id;
+        this.ownerUser = ownerUser;
+        this.borrowedBook = borrowedBook;
+        this.userBooks = userBooks;
+        this.loanDate = loanDate;
+        this.returnDate = returnDate;
+        this.closed = closed;
+        this.overdue = overdue;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getOwnerUser() {
+        return ownerUser;
+    }
+
+    public void setOwnerUser(User ownerUser) {
+        this.ownerUser = ownerUser;
+    }
+
+    public Book getBorrowenBook() {
+        return borrowedBook;
+    }
+
+    public void setBorrowenBook(Book borrowenBook) {
+        this.borrowedBook = borrowenBook;
+    }
+
+    public List<Book> getUserBooks() {
+        return userBooks;
+    }
+
+    public void setUserBooks(List<Book> userBooks) {
+        this.userBooks = userBooks;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public boolean isOverdue() {
+        return overdue;
+    }
+
+    public void setOverdue(boolean overdue) {
+        this.overdue = overdue;
+    }
+
 }
