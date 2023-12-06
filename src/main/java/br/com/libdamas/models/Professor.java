2e91e4de0d77082a4ac68833d6b7b22f0eb89d00
Marcos Filipe Capella <marcoscapella@outlook.com>
2023-12-06 16:52:44 -0300

    Creating constructors

--- src/main/java/br/com/libdamas/models/Professor.java
+++ src/main/java/br/com/libdamas/models/Professor.java
@@ -7,6 +7,16 @@ import jakarta.persistence.Entity;
 @DiscriminatorValue("professor")
 public class Professor extends User{
 
+    public Professor() {
+    }
+
+    public Professor(String name, String phoneNumber, String enrollment, String password) {
+        super.setName(name);
+        super.setPhoneNumber(phoneNumber);
+        super.setEnrollment(enrollment);
+        super.setPasssword(password);
+    }
+
     @Override
     public String toString() {
         return "Professor [name=" + super.getName() + ", phoneNumber=" + super.getPhoneNumber() + ", enrollment=" + super.getEnrollment() + "]";
--- src/main/java/br/com/libdamas/models/Student.java
+++ src/main/java/br/com/libdamas/models/Student.java
@@ -6,6 +6,17 @@ import jakarta.persistence.Entity;
 @Entity
 @DiscriminatorValue("student")
 public class Student extends User{
+    
+    public Student() {
+        // Default constructor
+    }
+    
+    public Student(String name, String phoneNumber, String enrollment, String password) {
+        super.setName(name);
+        super.setPhoneNumber(phoneNumber);
+        super.setEnrollment(enrollment);
+        super.setPasssword(password);
+    }
 
     @Override
     public String toString() {
