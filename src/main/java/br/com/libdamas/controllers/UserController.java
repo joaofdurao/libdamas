d12b3752c203feefa177049dd7e5fb3441c0c1ef
Marcos Filipe Capella <marcoscapella@outlook.com>
2023-12-06 16:53:30 -0300

    User Stories CAD1 CAD2

--- src/main/java/br/com/libdamas/controllers/UserController.java
+++ src/main/java/br/com/libdamas/controllers/UserController.java
@@ -2,6 +2,7 @@ package br.com.libdamas.controllers;
 
 import br.com.libdamas.DAO.UserDAO;
 import br.com.libdamas.models.User;
+import jakarta.validation.Valid;
 
 public class UserController {
 
@@ -9,6 +10,42 @@ public class UserController {
 
     public UserController() {
         this.userDAO = new UserDAO();
+    }
+
+    public void createUser(@Valid User user) {
+        // Fetch user if it exists
+        User testUser = this.getUserByEnrollment(user.getEnrollment());
+        // Check if user is new testUser is null
+        if (testUser == null) {
+            try {
+                userDAO.createInstance(user);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            // If user already exists it should show error message
+            System.out.println("Matrícula já existe");
+        }
+    }
+
+    public User getUserById(Long userId) {
+        return userDAO.findInstance(userId);
+    }
+
+    public User getUserByEnrollment(String enrollment) {
+        return userDAO.findByEnrollment(enrollment);
+    }
+
+    public User getUserByName(String partialName) {
+        return userDAO.findByName(partialName);
+    }
+
+    public void updateUser(Long userId, @Valid User user) {
+        userDAO.updateInstance(user, userId);
+    }
+
+    public void deleteUser(User user, Long userId) {
+        userDAO.deleteInstance(user, userId);
     }
 
     public boolean validateCredentials(String enrollment, String password) {
