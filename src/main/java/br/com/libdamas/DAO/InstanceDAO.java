4ee895f0cf5f3b15773e1c8e262ccad03bee5067
joaofdurao <devjoaofelipe.dlc@gmail.com>
2023-12-05 22:36:32 -0300

    Comentários adicionados.

--- src/main/java/br/com/libdamas/DAO/InstanceDAO.java
+++ src/main/java/br/com/libdamas/DAO/InstanceDAO.java
@@ -13,24 +13,29 @@ public abstract class InstanceDAO<T> {
         this.jpaUtils = new JPAUtils();
     }
 
+    //CREATE
     public void createInstance(T obj) {
         jpaUtils.executeInsideTransaction(entityManager -> entityManager.persist(obj));
     }
 
+    //FindByID
     public T findInstance(Integer id) {
         EntityManager entityManager = jpaUtils.getEntityManager();
         return entityManager.find(getEntityClass(), id);
     }
 
+    //FindAll
     public List<T> findAllInstance() {
         EntityManager entityManager = jpaUtils.getEntityManager();
         return entityManager.createQuery("FROM " + getEntityClass().getName(), getEntityClass()).getResultList();
     }
 
+    //Update
     public void updateInstance(T obj, Integer id) {
         jpaUtils.executeInsideTransaction(entityManager -> entityManager.merge(obj));
     }
 
+    //Delete
     public void deleteInstance(T obj, Integer id) {
         jpaUtils.executeInsideTransaction(entityManager -> entityManager.remove(obj));
     }
