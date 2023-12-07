1590d366ecba89df3b2869f980fc7637acceccac
joaofdurao <devjoaofelipe.dlc@gmail.com>
2023-12-07 00:06:58 -0300

    Metodo Adiar emprestimo criado.

--- src/main/java/br/com/libdamas/App.java
+++ src/main/java/br/com/libdamas/App.java
@@ -1,7 +1,11 @@
 package br.com.libdamas;
 
+import java.time.LocalDate;
+
 import br.com.libdamas.DAO.LoanDAO;
+import br.com.libdamas.DAO.UserDAO;
 import br.com.libdamas.models.Loan;
+import br.com.libdamas.models.Student;
 import br.com.libdamas.views.system.MainScreen;
 
 /**
@@ -13,8 +17,19 @@ public class App {
 
         // EntityManagerFactory emf = Persistence.createEntityManagerFactory("mariaDB");
         // new LoginScreen().setVisible(true);
+        Student studentTest = new Student();   
+        studentTest.setName("Teste");
+        studentTest.setEnrollment("456");
+        studentTest.setPasssword("123");
+        studentTest.setPhoneNumber("965472358");
+
+        UserDAO userDAOteste = new UserDAO();
+        userDAOteste.createInstance(studentTest);
+        
         Loan loanTest = new Loan();
         LoanDAO loanTeste = new LoanDAO();
+        loanTest.setUser(studentTest);
+        loanTest.setReturnDate(LocalDate.now());
         loanTeste.createInstance(loanTest);
         new MainScreen();
         
--- src/main/java/br/com/libdamas/views/loan/PostponeLoanScreen.java
+++ src/main/java/br/com/libdamas/views/loan/PostponeLoanScreen.java
@@ -8,56 +8,69 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 import br.com.libdamas.controllers.LoanController;
 import br.com.libdamas.models.Loan;
 
 public class PostponeLoanScreen extends JFrame {
-
-    private LoanController loanController = new LoanController();
+    private LoanController loanController;
+    private JTextField searchField;
+    private JTable resultsTable;
+    private JButton searchButton;
+    private JButton postponeButton; // Add the postpone button
+    private JPanel topPanel;
+    private JPanel bottomPanel; // Add the bottom panel
 
     public PostponeLoanScreen() {
-        initPostponeLoanScreenTemplate();
-        initComponents();
-        setVisible(true);
+        loanController = new LoanController();
 
-    }
-
-    public void initPostponeLoanScreenTemplate() {
-        setTitle("Postpone Loan");
-        setSize(500, 400);
+        setTitle("Postpone Loan Screen");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setSize(400, 180);
         setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
 
-    }
-
-    public void initComponents() {
-        addSearchField();
-    }
-
-    //SEARCH FIELD --------------------------------------------------------------
-    public void addSearchField() {
-        JPanel searchPanel = new JPanel();
-        JTextField searchField = new JTextField(20);
-        JButton searchButton = new JButton("Search");
-
+        // Search Panel --------------------------------------
+        topPanel = new JPanel();
+        searchField = new JTextField(20);
+        searchButton = new JButton("Search");
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 searchLoan(Long.parseLong(searchField.getText()));
             }
         });
+        topPanel.add(searchField);
+        topPanel.add(searchButton);
 
-        searchPanel.add(searchField);
-        searchPanel.add(searchButton);
+        // Results Table --------------------------------------
+        String[] columnNames = { "Loan ID", "User", "Loan Date", "Return Date", "Overdue" };
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+        resultsTable = new JTable(model);
 
-        add(searchPanel, BorderLayout.NORTH);
+        // Postpone Button --------------------------------------
+        bottomPanel = new JPanel();
+        postponeButton = new JButton("Postpone");
+        postponeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                executePospone(Long.parseLong(resultsTable.getValueAt(0, 0).toString()));
+            }
+        });
+        bottomPanel.add(postponeButton);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(new JScrollPane(resultsTable), BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
     }
 
     protected void searchLoan(Long loanId) {
-        
         Loan loan = loanController.findLoanById(loanId);
 
         if (loan == null) {
@@ -65,19 +78,48 @@ public class PostponeLoanScreen extends JFrame {
                     JOptionPane.ERROR_MESSAGE);
 
         } else {
-            JOptionPane.showMessageDialog(PostponeLoanScreen.this, loan.toString(), "Loan " + loan.getId(),
-                    JOptionPane.INFORMATION_MESSAGE);
+            updateTable(loan);
         }
 
     }
 
-    //RESULT FIELD --------------------------------------------------------------
+    protected void updateTable(Loan loan) {
+        DefaultTableModel model = (DefaultTableModel) resultsTable.getModel();
 
-    
+        if (model.getRowCount() > 0) {
+            model.removeRow(0);
+            model.addRow(
+                    new Object[] { loan.getId(), loan.getUser().getName(), loan.getLoanDate(), loan.getReturnDate(),
+                            loan.isOverdue() });
 
+        } else {
+            model.addRow(
+                    new Object[] { loan.getId(), loan.getUser().getName(), loan.getLoanDate(), loan.getReturnDate(),
+                            loan.isOverdue() });
+        }
 
+    }
 
+    protected void executePospone(Long loanId) {
+        String[] options = { "YES", "NO" };
+        int executePospone = JOptionPane.showOptionDialog(
+                PostponeLoanScreen.this, "Add more 15 days to the return date?", "Confirm postpone",
+                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
 
+        if (executePospone == 0) {
 
+            boolean postponed = loanController.postponeLoan(loanId);
+
+            if (postponed) {
+                updateTable(loanController.findLoanById(loanId));
+                JOptionPane.showMessageDialog(PostponeLoanScreen.this, "Loan postponed successfully.", "Success",
+                        JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(PostponeLoanScreen.this, "Error postponing loan.", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+    }
 
 }
